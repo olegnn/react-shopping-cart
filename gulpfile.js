cba0a02dc0b6baaa0feea2647dd817ea8c0929ce
olegnn <olegnosov1@gmail.com>
2016-09-27 12:06:02 +0300

    Docs generation corrected, docs updated

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,7 @@
 const gulp = require('gulp');
 const babel = require('gulp-babel');
 const uglify = require('gulp-uglify');
-const documentation = require('gulp-documentation');
+const exec = require('gulp-exec');
 const concat = require('gulp-concat');
 
 gulp.task('build', () =>
@@ -13,11 +13,9 @@ gulp.task('build', () =>
 
 gulp.task('doc', () => {
   gulp.src('./src/components/*.js')
-    .pipe(documentation({ shallow: true, format: 'md', filename: 'COMPONENTS.md' }))
-    .pipe(gulp.dest('docs'));
+    .pipe(exec('./node_modules/documentation/bin/documentation.js build -f=md --shallow=true ./src/components -o ./docs/COMPONENTS.md'));
   gulp.src('./src/types.js')
-    .pipe(documentation({ shallow: true, format: 'md', filename: 'TYPES.md' }))
-    .pipe(gulp.dest('docs'));
+    .pipe(exec('./node_modules/documentation/bin/documentation.js build -f=md --shallow=true ./src/types.js -o ./docs/TYPES.md'));
   gulp.src(['./docs/MAIN.md', './docs/COMPONENTS.md', './docs/TYPES.md', './docs/DEVELOPMENT.md'])
     .pipe(concat('README.md'))
     .pipe(gulp.dest('.'));
