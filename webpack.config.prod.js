98a6eee37f81c9c80ff1946adcbb0b5200ce610b
Oleg Nosov <olegnosov1@gmail.com>
2021-02-15 15:21:48 +0300

    Fix #14

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -24,6 +24,7 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "production.js",
+    globalObject: "this",
     library: "react-shopping-cart",
     libraryTarget: "umd",
   },
